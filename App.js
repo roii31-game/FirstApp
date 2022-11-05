03e22a87fdb1f9ee6a42b4aa77194107ab0a1948
roii31-game <74428415+roii31-game@users.noreply.github.com>
2022-11-04 22:22:00 -0500

    First Commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
+  console.log("App Executed");
+
+  // let x;
+  // x.toString();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Todays's Tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go */}
+          <Task text="Task 1"></Task>
+          <Task text="Task 2"></Task>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +27,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
+  },
+
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
   },
 });
